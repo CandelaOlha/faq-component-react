c783025f7d0e7556f6c05e753f5a7dd315765bc8
“c.b.olha” <c.b.olha@viseven.com>
2023-11-14 16:10:53 -0300

    added logic

--- src/components/FaqContainer.jsx
+++ src/components/FaqContainer.jsx
@@ -1,45 +1,65 @@
 import "../styles/FaqContainer.css";
 import QuestionBlock from "./QuestionBlock";
+import { useState } from "react";
 
-const faqs = [
+const initialFaqs = [
   {
     question: "Question 1",
     answer:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis cupiditate accusamus veritatis sapiente quisquam repudiandae ullam vero blanditiis. Maxime, nostrum!",
+    active: false,
     id: 1,
   },
   {
     question: "Question 2",
     answer:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis cupiditate accusamus veritatis sapiente quisquam repudiandae ullam vero blanditiis. Maxime, nostrum!",
+    active: false,
     id: 2,
   },
   {
     question: "Question 3",
     answer:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis cupiditate accusamus veritatis sapiente quisquam repudiandae ullam vero blanditiis. Maxime, nostrum!",
+    active: false,
     id: 3,
   },
   {
     question: "Question 4",
     answer:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis cupiditate accusamus veritatis sapiente quisquam repudiandae ullam vero blanditiis. Maxime, nostrum!",
+    active: false,
     id: 4,
   },
   {
     question: "Question 5",
     answer:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis cupiditate accusamus veritatis sapiente quisquam repudiandae ullam vero blanditiis. Maxime, nostrum!",
+    active: false,
     id: 5,
   },
 ];
 
 const FaqContainer = () => {
+  const [faqs, setFaqs] = useState(initialFaqs);
+
+  const toggleQuestion = (id) => {
+    const newArray = faqs.map((faq) => {
+      if (faq.id === id) {
+        faq.active = !faq.active;
+      } else {
+        faq.active = false;
+      }
+      return faq;
+    });
+    setFaqs(newArray);
+  };
+
   return (
     <div className="faq-container">
       <h1 className="headline">FAQ Container</h1>
       {faqs.map((faq) => (
-        <QuestionBlock faq={faq} key={faq.id} />
+        <QuestionBlock faq={faq} key={faq.id} toggleQuestion={toggleQuestion} />
       ))}
     </div>
   );
--- src/components/QuestionBlock.jsx
+++ src/components/QuestionBlock.jsx
@@ -1,22 +1,25 @@
 import "../styles/QuestionBlock.css";
 import PropTypes from "prop-types";
 
-const QuestionBlock = ({ faq }) => {
-  const { question, answer, id } = faq;
+const QuestionBlock = ({ faq, toggleQuestion }) => {
+  const { question, answer, active, id } = faq;
 
   return (
     <div className="question">
       <header className="question-header">
         <h2 className="question-header-text">{question}</h2>
-        <button className="toggle-button">+</button>
+        <button onClick={() => toggleQuestion(id)} className="toggle-button">
+          +
+        </button>
       </header>
-      <p className="answer">{answer}</p>
+      <p className={`answer ${active && "active"}`}>{answer}</p>
     </div>
   );
 };
 
 QuestionBlock.propTypes = {
   faq: PropTypes.object.isRequired,
+  toggleQuestion: PropTypes.func.isRequired,
 };
 
 export default QuestionBlock;
